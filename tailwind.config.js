8dd0d511ba9175f95bd93683e6d0e780544e8b81
Chris McGee <chris@chrismcgee.info>
2022-07-12 10:08:45 -0400

    🔨 Gets TailwindCSS running without Parcel and PostCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./js/**/*.js"],
+  content: [
+    "./index.html",
+    "./js/main.js"
+  ],
   theme: {
     extend: {},
   },
