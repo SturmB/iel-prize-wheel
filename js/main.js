74c7036dcade4357da862dca2d915038951101b2
Chris McGee <chris@chrismcgee.info>
2022-07-12 23:50:40 -0400

    ✨ Further refactors and adds gradients

--- js/main.js
+++ js/main.js
@@ -1,7 +1,46 @@
 /* eslint-disable no-undef */
 
-const t2Percent = 25;
-const numWedgesPerTier = 5;
+// Define gradients (optional)
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const canvasCenter = canvas.height / 2;
+
+const level1Gradient = ctx.createRadialGradient(
+  canvasCenter,
+  canvasCenter,
+  50,
+  canvasCenter,
+  canvasCenter,
+  250
+);
+level1Gradient.addColorStop(0, "#00d4ff");
+level1Gradient.addColorStop(1, "#007894");
+const level2Gradient = ctx.createRadialGradient(
+  canvasCenter,
+  canvasCenter,
+  50,
+  canvasCenter,
+  canvasCenter,
+  250
+);
+level2Gradient.addColorStop(0, "#d000ff");
+level2Gradient.addColorStop(1, "#8f00af");
+
+// Define wedges and levels
+const numWedgesPerLevel = 5;
+const strokeColor = "#4c005d";
+const levels = [
+  {
+    bgColor: level1Gradient,
+    percent: 75,
+    textColor: "#FFFFFF",
+  },
+  {
+    bgColor: level2Gradient,
+    percent: 25,
+    textColor: "#FFFFFF",
+  },
+];
 
 const audio = new Audio("/js/vendor/javascript-winwheel-2.8.0/tick.mp3");
 
@@ -12,18 +51,23 @@ const playSound = () => {
   audio.play();
 };
 
-const t2Size = winwheelPercentToDegrees(t2Percent / numWedgesPerTier);
+let highestLevelSize = 0;
 const segmentArray = [];
-for (let i = 0; i < numWedgesPerTier; i++) {
-  segmentArray.push({
-    fillStyle: "#beff5a",
-    text: "Tier 1"
-  });
-  segmentArray.push({
-    fillStyle: "#37ff00",
-    size: t2Size,
-    text: "Tier 2"
-  });
+const segments = levels.map((level, index) => {
+  return {
+    fillStyle: level.bgColor,
+    ...(index !== 0 && {
+      size: (highestLevelSize = winwheelPercentToDegrees(
+        level.percent / numWedgesPerLevel
+      )),
+    }),
+    strokeStyle: strokeColor,
+    text: `LEVEL\n${index + 1}`,
+    textFillStyle: level.textColor,
+  };
+});
+for (let i = 0; i < numWedgesPerLevel; i++) {
+  segmentArray.push(...segments);
 }
 
 const theWheel = new Winwheel({
@@ -34,19 +78,19 @@ const theWheel = new Winwheel({
     spins: 4,
     type: "spinToStop",
   },
-  numSegments: numWedgesPerTier * 2,
   lineWidth: 3,
+  numSegments: numWedgesPerLevel * levels.length,
   outerRadius: 300,
   pins: {
     fillStyle: "silver",
     number: 40,
     outerRadius: 4,
   },
-  rotationAngle: t2Size / 2,
+  rotationAngle: highestLevelSize / 2,
   segments: segmentArray,
   textAlignment: "outer",
   textMargin: 50,
-  textOrientation: "vertical",
+  textOrientation: "curved",
 });
 
 // eslint-disable-next-line no-unused-vars
